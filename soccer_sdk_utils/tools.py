6f991c2e9b09b60ee632c1708b65bb5c9381870e
Omar Crosby <omar.crosby@gmail.com>
2022-12-31 01:29:09 -0500

    fix(tools): adding anchor utility functions

--- soccer_sdk_utils/tools.py
+++ soccer_sdk_utils/tools.py
@@ -45,3 +45,40 @@ def urljoin(base: str | None, path: str | None) -> str:
         path = path[1:]
 
     return str(f"{base}/{path}")
+
+def get_href_from_anchor(anchor):
+    if anchor is None:
+        return None
+
+    if anchor.name != "a":
+        anchor = anchor.find("a")
+
+    if anchor is None:
+        return None
+
+    href = anchor.get("href")
+    if href is None:
+        return None
+
+    href = href.strip()
+    if len(href) == 0:
+        return None
+
+    return href
+
+
+def get_text_from_anchor(anchor):
+    if anchor is None:
+        return None
+
+    if anchor.name != "a":
+        anchor = anchor.find("a")
+
+    if anchor is None:
+        return None
+
+    text = anchor.text.strip()
+    if len(text) == 0:
+        return None
+
+    return text
